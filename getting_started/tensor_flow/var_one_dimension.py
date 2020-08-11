1cb2bc985f152edb49824af11473e86a701b69b6
DestrosCMC <64284439+DestrosCMC@users.noreply.github.com>
2020-08-11 09:16:26 -0400

    Create var_one_dimension.py

---
+++ getting_started/tensor_flow/var_one_dimension.py
@@ -0,0 +1,12 @@
+from tensorflow import Variable
+
+A1 = Variable([1, 2, 3, 4])
+
+# Print the variable A1
+print(A1)
+
+# Convert A1 to a numpy array and assign it to B1
+B1 = A1.numpy()
+
+# Print B1
+print(B1)
