6a554807656a2fac8ac53a12f2848dea42d4ad03
DestrosCMC <64284439+DestrosCMC@users.noreply.github.com>
2020-08-11 09:29:57 -0400

    Create mat_mul.py

---
+++ getting_started/tensor_flow/mat_mul.py
@@ -0,0 +1,12 @@
+from tensorflow import matmul, constant
+
+features = constant([[2, 24], [2, 26], [2, 57], [1, 37]])
+params = constant([[1000], [150]])
+bill = constant([[3913], [2682], [8617], [64400]])
+
+# Compute billpred using features and params
+billpred = matmul(features, params)
+
+# Compute and print the error
+error = bill - billpred
+print(error.numpy())
